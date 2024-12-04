2e2dee8c08707b16f0b5b143e9e2f18d937dea43
jaimartz <martinezgarridojairo@gmail.com>
2024-12-04 19:13:51 +0100

    First commit

---
+++ src/main/java/dev/jaimartz/pdf_manager/PdfManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.jaimartz.pdf_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfManagerApplication.class, args);
+	}
+
+}
